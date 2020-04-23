ed3b6d342d579b4e89f3e096b9b1fcc847d56640
tonarerenato <tonarerenato@yahoo.com.br>
2020-04-23 15:20:45 -0300

    Obtendo um post por id

---
+++ src/main/java/com/renato/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.renato.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renato.workshopmongo.domain.Post;
+import com.renato.workshopmongo.repository.PostRepository;
+import com.renato.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
